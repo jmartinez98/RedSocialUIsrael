5f0f90c26fc4a60ec09320c828e79cbbe2121130
Martinez <60410536+javier-or@users.noreply.github.com>
2020-08-06 15:00:17 -0500

    Perfin V1

---
+++ app/src/main/java/com/uisrael/redsocialuisrael/MyApplication.java
@@ -0,0 +1,22 @@
+package com.uisrael.redsocialuisrael;
+
+public class MyApplication {
+    private int idUsuario;
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+}
